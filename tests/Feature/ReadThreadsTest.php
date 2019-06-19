10a7dfe0c3bc04eaae5ca671677f8c7b09759719
Mahmudur Rahman <mahmudur.rahman@sslwireless.com>
2019-06-19 17:57:32 +0600

    04-A User May Respond to Threads

--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -33,7 +33,8 @@ class ReadThreadsTest extends TestCase
     public function a_user_can_read_replies_that_are_associated_with_a_thread()
     {
         $reply = factory('App\Reply')->create(['thread_id'=>$this->thread->id]);
-        $response = $this->get('/threads/'.$this->thread->id)
+
+        $response = $this->get($this->thread->path())
             ->assertSee($reply->body);
 
     }
