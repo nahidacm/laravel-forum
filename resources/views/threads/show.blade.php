b95827090e0dfe84287066b72aa95051869b5895
Mahmudur Rahman <mahmudur.rahman@sslwireless.com>
2019-06-20 11:35:24 +0600

    05-The Reply Form

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -22,5 +22,22 @@
                 @endforeach
             </div>
         </div>
+
+        @if(auth()->check())
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <form method="post" action="{{ $thread->path().'/replies' }}">
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <textarea class="form-control" name="body" id="body" placeholder="Have something to say?" rows="5"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+            </div>
+        </div>
+            @else
+            <p class="text-center">Please <a href="{{ route('login') }}">login</a> to participate in conversation.</p>
+        @endif
+
     </div>
 @endsection
